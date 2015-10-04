7eddd3dcee9e23a6c16a40c6da10823325092348
kovloq <perdana_adhitama@yahoo.com>
2015-10-04 20:57:32 +0900

    Final release

--- alkwangju/YoutubeViewController.swift
+++ alkwangju/YoutubeViewController.swift
@@ -20,17 +20,15 @@ class YoutubeViewController: UIViewController {
         super.viewDidLoad()
         let screenSize: CGRect = UIScreen.mainScreen().bounds
         let screenWidth = screenSize.width
-//        println(screenWidth)
+        
         let screenHeight = screenSize.height
 //        println(screenHeight)
-        var html="<html><body style=\"(margin:0)\"><embed width=\"\(screenWidth)\" height=\"\(screenHeight)\" src=\"https://www.youtube.com/embed/"+website!+"?rel=0&amp;autoplay=1&amp;showinfo=0\" frameborder=\"0\" allowfullscreen></embed></body></html>"
+        var html="<html><body style=\"margin:0;padding:0\"><embed align=\"center\" style=\"width:100%;height:90%\" src=\"https://www.youtube.com/embed/"+website!+"?rel=0&amp;autoplay=1&amp;showinfo=0\" frameborder=\"0\" allowfullscreen></embed></body></html>"
         webview.loadHTMLString(html, baseURL: nil)
 
        nav.titleTextAttributes=[NSFontAttributeName: UIFont(name: "Helvetica", size: 12)!]
 //        nav.topItem?.title=judul
         nav.topItem?.title=" "
-
-       
         
     }
     override func didReceiveMemoryWarning() {
@@ -43,6 +41,34 @@ class YoutubeViewController: UIViewController {
         dismissViewControllerAnimated(true, completion: nil) 
         println("backpressed")
     }
+    
+//    override func didRotateFromInterfaceOrientation(fromInterfaceOrientation: UIInterfaceOrientation) {
+//        var text=""
+//        switch UIDevice.currentDevice().orientation{
+//        case .Portrait:
+//            let screenSize: CGRect = UIScreen.mainScreen().bounds
+//            let screenWidth = screenSize.width
+//            println(screenWidth)
+//            text="Portrait"
+//        case .PortraitUpsideDown:
+//            text="PortraitUpsideDown"
+//        case .LandscapeLeft:
+//            let screenSize: CGRect = UIScreen.mainScreen().bounds
+//            let screenWidth = screenSize.width
+//            println(screenWidth)
+//           
+//            text="LandscapeLeft"
+//        case .LandscapeRight:
+//            let screenSize: CGRect = UIScreen.mainScreen().bounds
+//            let screenWidth = screenSize.width
+//            println(screenWidth)
+//            text="LandscapeRight"
+//        default:
+//            text="Another"
+//        }
+//        NSLog("You have moved: \(text)")        
+//    }
+
     
 
 }
